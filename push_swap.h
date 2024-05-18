887379322573fc2dd0837686a2e269e8f8d97440
Daniel Kremer <dkremer@1-E-4.42heilbronn.de>
2024-05-18 15:49:45 +0200

    organized h-file
    removed unused functions also added comments and made file norminette
    conform

--- push_swap.h
+++ push_swap.h
@@ -6,55 +6,88 @@
 /*   By: dkremer <dkremer@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 18:36:43 by dkremer           #+#    #+#             */
-/*   Updated: 2024/04/20 16:32:43 by dkremer          ###   ########.fr       */
+/*   Updated: 2024/05/17 16:47:56 by dkremer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 # include "libft/include/libft.h"
+# include <limits.h>
 # include <stdbool.h>
 
 typedef struct s_list
 {
 	int				value;
 	int				index;
+	int				price;
+	bool			a_med;
+	bool			cheapest;
+	struct s_list	*target;
 	struct s_list	*next;
 	struct s_list	*prev;
 }					t_list;
 
+// Checking the Arguments
+
+void				ft_check_args(int argc, char **argv);
 bool				is_sorted(t_list **stack);
-void				free_list(char **str);
+long				ft_atol(const char *str);
+
+// Error and Free Functions
+
 void				error(void);
+void				free_list(char **str);
 void				free_stack(t_list **stack);
 void				free_stacks(t_list **a, t_list **b);
+
+// Instructions
+
+int					swap(t_list **stack);
+int					sa(t_list **stack_a);
+int					sb(t_list **stack_b);
+int					ss(t_list **stack_a, t_list **stack_b);
+int					rotate(t_list **stack);
+int					ra(t_list **stack_a);
+int					rb(t_list **stack_b);
+int					rr(t_list **stack_a, t_list **stack_b);
+int					reverse_rotate(t_list **stack);
+int					rra(t_list **stack_a);
+int					rrb(t_list **stack_b);
+int					rrr(t_list **stack_a, t_list **stack_b);
+int					push(t_list **stack_to, t_list **stack_from);
+int					pa(t_list **stack_a, t_list **stack_b);
+int					pb(t_list **stack_a, t_list **stack_b);
+
+// List functions
+
 t_list				*ft_lstnew(int value);
-void				ft_lstadd_front(t_list **stack, t_list *new);
 t_list				*ft_lstlast(t_list *head);
-void				ft_lstadd_back(t_list **stack, t_list *new);
+void				ft_lstadd_back(t_list **stack, t_list *new_node);
 int					ft_listsize(t_list *head);
 void				print_list(t_list *head);
-//int  ft_isnum(char *num);
-//int  ft_contains(int num, char **args, int i);
-void	ft_check_args(int argc, char **argv);
-void    lst_addback(t_list **root, int value);
-int swap(t_list **stack);
-int sa(t_list **stack_a);
-int sb(t_list **stack_b);
-int ss(t_list **stack_a, t_list **stack_b);
-int rotate(t_list **stack);
-int ra(t_list **stack_a);
-int rb(t_list **stack_b);
-int rr(t_list **stack_a, t_list **stack_b);
-int reverse_rotate(t_list **stack);
-int rra(t_list **stack_a);
-int rrb(t_list **stack_b);
-int rrr(t_list **stack_a, t_list **stack_b);
-int push(t_list **stack_to, t_list **stack_from);
-int pa(t_list **stack_a, t_list **stack_b);
-int pb(t_list **stack_a, t_list **stack_b);
-int	get_distance(t_list **stack, int index);
-void	stack_index(t_list **stack);
-void    simple_sort(t_list **a, t_list **b);
+
+// Sorting Functions
+
+void				sort_3(t_list **a);
+void				handle_5(t_list **a, t_list **b);
+void				fast_sort(t_list **a, t_list **b);
+
+// Sorting util functions
+
+void				init_nodes_a(t_list *a, t_list *b);
+void				init_nodes_b(t_list *a, t_list *b);
+void				get_price_a(t_list *a, t_list *b);
+void				get_price_b(t_list *a, t_list *b);
+void				set_target_a(t_list *a, t_list *b);
+void				set_target_b(t_list *a, t_list *b);
+t_list				*cheapest(t_list *b);
+t_list				*smallest(t_list *stack);
+t_list				*biggest(t_list *stack);
+void				init_index(t_list *stack);
+void				move_a(t_list **a, t_list **b);
+void				move_a(t_list **a, t_list **b);
+void				finish_rotation_a(t_list **a, t_list **b, t_list *cheap);
+void				finish_rotation_b(t_list **a, t_list **b, t_list *cheap);
 
 #endif // !PUSH_SWAP_H
