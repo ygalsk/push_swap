3f151819a07131a4327048d977108f2440bb4173
Daniel Kremer <dkremer@1-E-4.42heilbronn.de>
2024-05-18 15:56:57 +0200

    norminette, small sort and use of atol
    madefile norminetteconform by using the c-formatter_42. Also added the
    functions to handle 3 and 5 numbers also changed atoi to atol

--- main.c
+++ main.c
@@ -6,12 +6,12 @@
 /*   By: dkremer <dkremer@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:54:37 by dkremer           #+#    #+#             */
-/*   Updated: 2024/04/21 15:40:42 by dkremer          ###   ########.fr       */
+/*   Updated: 2024/05/17 16:50:41 by dkremer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/include/libft.h"
 #include "push_swap.h"
+#include <sys/wait.h>
 
 static void	init_stacks(t_list **a, int argc, char **argv)
 {
@@ -32,7 +32,7 @@ static void	init_stacks(t_list **a, int argc, char **argv)
 		j = 0;
 		while (args[j])
 		{
-			new = ft_lstnew(ft_atoi(args[j]));
+			new = ft_lstnew(ft_atol(args[j]));
 			ft_lstadd_back(a, new);
 			j++;
 		}
@@ -41,12 +41,56 @@ static void	init_stacks(t_list **a, int argc, char **argv)
 	}
 }
 
+void	sort_3(t_list **a)
+{
+	if (biggest(*a)->value == (*a)->value)
+	{
+		ra(a);
+		if (smallest(*a)->value != (*a)->value)
+			sa(a);
+	}
+	else if (smallest(*a)->value == (*a)->value)
+	{
+		rra(a);
+		sa(a);
+	}
+	else if (biggest(*a)->value == ft_lstlast(*a)->value)
+		sa(a);
+	else
+		rra(a);
+}
+
+void	handle_5(t_list **a, t_list **b)
+{
+	while (ft_listsize(*a) > 3)
+	{
+		init_index(*a);
+		if (smallest(*a)->a_med)
+		{
+			while (smallest(*a)->index != (*a)->index)
+				ra(a);
+		}
+		else
+			while (smallest(*a)->index != (*a)->index)
+				rra(a);
+		pb(a, b);
+	}
+	if (!is_sorted(a))
+		sort_3(a);
+	while (*b)
+		pa(a, b);
+}
+
 static void	sort(t_list **a, t_list **b)
 {
-	if (ft_listsize(*a) <= 5)
-		simple_sort(a, b);
-	//else
-	//	algo(a, b);
+	if (ft_listsize(*a) == 2)
+		sa(a);
+	else if (ft_listsize(*a) == 3)
+		sort_3(a);
+	else if (ft_listsize(*a) == 5)
+		handle_5(a, b);
+	else
+		fast_sort(a, b);
 }
 
 int	main(int argc, char **argv)
@@ -54,8 +98,7 @@ int	main(int argc, char **argv)
 	t_list	**a;
 	t_list	**b;
 
-	(void)argv;
-	if (argc <= 2)
+	if (argc < 2)
 		return (0);
 	ft_check_args(argc, argv);
 	a = ft_calloc(1, sizeof(t_list));
@@ -63,14 +106,8 @@ int	main(int argc, char **argv)
 	*a = NULL;
 	*b = NULL;
 	init_stacks(a, argc, argv);
-	print_list(*a);
-	if (is_sorted(a))
-	{
-		free_stacks(a, b);
-		return (0);
-	}
-	sort(a, b);
-	print_list(*a);
+	if (!(is_sorted(a)))
+		sort(a, b);
 	free_stacks(a, b);
 	return (0);
 }
