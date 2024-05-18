fbb5028db5a35a67bf06659de914b1da431cb479
Daniel Kremer <dkremer@1-E-4.42heilbronn.de>
2024-05-18 17:16:39 +0200

    added the logic of sorting algo(turk approach)

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: dkremer <dkremer@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 15:36:02 by dkremer           #+#    #+#             */
-/*   Updated: 2024/05/18 17:12:48 by dkremer          ###   ########.fr       */
+/*   Updated: 2024/05/18 17:16:04 by dkremer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,39 @@ static void	rr_both(t_list **a, t_list **b, t_list *cheap)
 		rrr(a, b);
 	init_index(*a);
 	init_index(*b);
+}
+
+void	fast_sort(t_list **a, t_list **b)
+{
+	t_list	*smallest_node;
+	int		len_a;
+
+	len_a = ft_listsize(*a);
+	if (len_a == 5)
+		handle_5(a, b);
+	else
+	{
+		while (len_a-- > 3)
+			pb(a, b);
+	}
+	if (!is_sorted(a))
+		sort_3(a);
+	while (ft_listsize(*a) > 1)
+	{
+		init_nodes_a(*a, *b);
+		move_b(a, b);
+	}
+	while (*b)
+	{
+		init_nodes_b(*a, *b);
+		move_a(a, b);
+	}
+	init_index(*a);
+	smallest_node = smallest(*a);
+	if (smallest_node->a_med)
+		while (*a != smallest_node)
+			ra(a);
+	else
+		while (*a != smallest_node)
+			rra(a);
 }
