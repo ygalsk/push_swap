024ba9c1f1896fa742df3a6d014038117e758b05
Daniel Kremer <dkremer@1-E-4.42heilbronn.de>
2024-05-18 15:51:37 +0200

    madef ile norminette conform and also deleted unused fucntions
    used c-formatter-42 to make file norminette conform

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: dkremer <dkremer@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 18:45:35 by dkremer           #+#    #+#             */
-/*   Updated: 2024/04/19 15:49:50 by dkremer          ###   ########.fr       */
+/*   Updated: 2024/05/17 16:51:24 by dkremer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,10 @@ bool	is_sorted(t_list **stack)
 	while (head && head->next)
 	{
 		if (head->value > head->next->value)
-			return (true);
+			return (false);
 		head = head->next;
 	}
-	return (false);
+	return (true);
 }
 
 void	free_stack(t_list **stack)
@@ -62,60 +62,4 @@ void	free_stacks(t_list **a, t_list **b)
 {
 	free_stack(a);
 	free_stack(b);
-}
-
-int	get_distance(t_list **stack, int index)
-{
-	t_list *head;
-	int	dist;
-
-	dist = 0;
-	head = *stack;
-	while (head)
-	{
-		if (head->index == index)
-			break;
-		dist++;
-		head = head->next;
-	}
-	return (dist);
-}
-
-static void	*next_min(t_list **stack)
-{
-	t_list *head;
-	t_list *min;
-	int	checker;
-
-	min = NULL;
-	checker = 0;
-	head = *stack;
-	if (head)
-	{
-		while (head)
-		{
-			if ((head->index == -1) && (!checker || head->value < min->value))
-			{
-				min = head;
-				checker = 1;
-			}
-			head = head->next;
-		}
-	}
-	return (min);
-}
-
-void	stack_index(t_list **stack)
-{
-	t_list *head;
-	int	index;
-
-	index = 0;
-	head = next_min(stack);
-	while (head)
-	{
-		head->index = index ++;
-		head = next_min(stack);
-	}
-	
 }
