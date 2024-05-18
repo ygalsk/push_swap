33711fc566a2fde992f1f9ee90dc5218895ea8c6
Daniel Kremer <dkremer@1-E-4.42heilbronn.de>
2024-05-18 15:43:50 +0200

    added atol
    added atol and using it instead of atoi so i can handle INT_MIN and
    INT_MAX

---
+++ atol.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atol.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dkremer <dkremer@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 14:58:25 by dkremer           #+#    #+#             */
+/*   Updated: 2024/05/17 16:49:47 by dkremer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+long	ft_atol(const char *str)
+{
+	int		i;
+	int		sgn;
+	long	res;
+
+	i = 0;
+	sgn = 1;
+	res = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sgn = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = (res * 10) + (str[i] - '0');
+		i++;
+	}
+	return (res * sgn);
+}
